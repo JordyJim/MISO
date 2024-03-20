b8985dc2cc5ff1df20585bc81c8bae0a00287b68
Jordy Morales <jlacr20@ucvvirtual.edu.pe>
2024-03-20 14:46:46 -0500

    update-schema

--- src/index.js
+++ src/index.js
@@ -8,3 +8,4 @@ function prueba2(){
     location.href = 'https://api.whatsapp.com/send?phone=51941519960&text=Buen%20d%C3%ADa,%20quisiera%20informaci%C3%B3n%20sobre%20los%20m%C3%A9todos,%20precios%20y%20soluciones%20ofrecidos%20en%20MISO%20PERU';
   console.log('Test de texto :');
 }
+
