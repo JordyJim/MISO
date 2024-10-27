76bcbfb9e406620491e2e0faa79b5bc3111b9d85
Jordy Morales <67881496+JordyJim@users.noreply.github.com>
2024-10-27 14:47:06 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 function prueba(){
     var valor = document.getElementById('input-txt').value;
-    location.href = 'https://api.whatsapp.com/send?phone=51960166549&text='+valor;
+    location.href = 'https://api.whatsapp.com/send?phone=51906993457&text='+valor;
   console.log('Test de texto :',valor);
 }
 
 function prueba2(){
-    location.href = 'https://api.whatsapp.com/send?phone=51960166549&text=Buen%20d%C3%ADa,%20quisiera%20informaci%C3%B3n%20sobre%20los%20m%C3%A9todos,%20precios%20y%20soluciones%20ofrecidos%20en%20MISO%20PERU';
+    location.href = 'https://api.whatsapp.com/send?phone=51906993457&text=Buen%20d%C3%ADa,%20quisiera%20informaci%C3%B3n%20sobre%20los%20m%C3%A9todos,%20precios%20y%20soluciones%20ofrecidos%20en%20MISO%20PERU';
   console.log('Test de texto :');
 }
 
